b3381030ae3fcd8193ea46171ed342ec027e62bd
salinaj27 <salinajohn80@gmail.com>
2023-03-10 08:40:49 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import "bootstrap/dist/css/bootstrap.css";
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
